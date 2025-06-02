ea1316a074e5be79c0d35cf7c0be15bda8ccd528
The Jared Wilcurt <4629794+TheJaredWilcurt@users.noreply.github.com>
2025-06-01 23:00:58 -0400

    Runner

--- src/runner.js
+++ src/runner.js
@@ -1,4 +1,4 @@
-import { setupDevtools } from './src/devtools.js';
+import { setupDevtools } from './devtools.js';
 
 if (process.env.NODE_ENV === 'development' || window.__VUE_PROD_DEVTOOLS__) {
   setupDevtools();
